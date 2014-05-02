41a0cae9bc4e5b61f60d78d220671712e72db37a
ganmor <morgan.laupies@gmail.com>
2014-05-02 17:45:47 +0200

    first working draft

--- examples/ScrollablePeople.js
+++ examples/ScrollablePeople.js
@@ -1,6 +1,27 @@
-define(['rawgithub/src/js/backbone-infinite-list'], function (InfiniteList) {
+/*global define */
+define([
+	'rawgithub/src/js/backbone-infinite-list',
+	'rawgithub/examples/datasets/people',
+	'tpl!rawgithub/examples/html/people.html'
+], function (InfiniteList,
+				  peopleData,
+				  peopleTpl) {
+
+	'use strict';
 
 	var ScrollablePeople = InfiniteList.extend({
+
+		getSingleElementTemplate : function () {
+			return peopleTpl;
+		},
+
+		getData : function () {
+			return peopleData.toJSON();
+		},
+
+		displayNewElementAlert : function () {
+
+		}
 
 
 	});
