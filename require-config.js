26d7ebf9b5208113751bde761e94ca757b93bebd
ganmor <morgan.laupies@gmail.com>
2014-05-02 17:49:46 +0200

    path

--- require-config.js
+++ require-config.js
@@ -1,6 +1,6 @@
 var require = {
 	 paths: {
-			"rawgithub": "https://rawgithub.com/ganmor/backbone-infinite-list/master/"
+			"rawgithub": "https://rawgithub.com/ganmor/backbone-infinite-list/master"
 		   /*  "rawgithub": "http://192.168.10.30:90/infinite-scroll-src"  */
 		   /* "rawgithub": "http://localhost:90/infinite-scroll-src" */
 	 },
